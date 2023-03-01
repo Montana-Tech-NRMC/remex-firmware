959d1d95133137c7fc305e20c073c19d6ba8e398
Justin Bak <tkdefender@protonmail.com>
2023-03-01 11:13:48 -0700

    setup for new pin layout and i2c setup

--- remex.c
+++ remex.c
@@ -12,88 +12,28 @@
 #include "hardwareIO.h"
 #include "i2c_memory_map.h"
 
-#define RING_BUF_SIZE 10
-
 /**
  * remex.c
  */
-/////// Global Vars /////
-
-static const int deadZone = 5;
-int gainxMult, gainxDiv, intxMult, intxDiv;
-
-int integrator_ring_buf[RING_BUF_SIZE];
-unsigned int integrator_ptr = 0;
-/////////////////////////
-
-void pushIntegratorVal(int val) {
-    integrator_ring_buf[integrator_ptr] = (val / RING_BUF_SIZE);
-    integrator_ptr++;
-    if (integrator_ptr >= RING_BUF_SIZE) {
-        integrator_ptr = 0;
-    }
-}
-
-int integratorRollingAverage() {
-    int sum = 0;
-    unsigned int i;
-    for (i = RING_BUF_SIZE; i != 0; i--) {
-        sum += integrator_ring_buf[i-1];
-    }
-    return sum;
-}
 
 // Must have definition. regmap is set in the init method
 unsigned char regmap[REGMAP_SIZE] = { 0x00 };
 
 enum remex_states remex;
-int speedA = 0;
-
-void adc_channel_a(int current) {
-    regmap[ADC_A_L] = current & 0xFF; // low byte
-    regmap[ADC_A_H] = current >> 8;   // high byte
-    return;
-}
-
-void init_timer() {
-    TB1CCTL0 |= CCIE;                // TBCCR0 Interrupt Enabled
-    TB1CCR0 = 50000;
-    TB1CTL = TBSSEL__SMCLK | MC__UP | ID_3; // Use the SMCLCK in Up Mode
-}
-
-void switch_cb_zero(int state) {
-    remex = zeroed;
-    splitInt(POSITION_A_L, 0);
-    P4OUT ^= BIT7;
-}
-
-void switch_cb_upper(int state) {
-    remex = halt;
-    set_PWM_A(0);
-}
-
-int positionA;
 
 // init is called once at the beginning of operation.
 void init(void)
 {
     clear_registers();
-    i2c_slave_init(onI2CStartBit, onI2CStopBit, onI2CByteReceived, onI2CByteTransmit, SLAVE_ADDR);
-    init_i2c_memory_map((unsigned char *)&regmap, onI2CCommand);
-    init_PWM_A();
-    //init_encoders(&positionA, 0);
-    init_encoders(((unsigned int *)&regmap[POSITION_A_L]), 0);
-    init_switches(switch_cb_upper, switch_cb_zero);
+    init_adc(0L, 0L, 0L);
+    //i2c_slave_init(onI2CStartBit, onI2CStopBit, onI2CByteReceived, onI2CByteTransmit, SLAVE_ADDR);
+    //init_i2c_memory_map((unsigned char *)&regmap, onI2CCommand);
+
     __bis_SR_register(GIE); // Enable global interrupts
 
-    intxMult = 1;
-    intxDiv = 5;
-    gainxMult = 1;
-    gainxDiv = 50;
-
 	// Turn on proof of life LED.
-    P4DIR |= BIT7;
-    P4OUT |= BIT7;
+    P3DIR |= BIT5;
+    P3OUT |= BIT5;
     // Disable GPIO High impedance.
     PM5CTL0 &=~ LOCKLPM5;
 
@@ -102,58 +42,16 @@ void init(void)
 // code within loop repeats continually.
 void loop(void)
 {
-    int pval, ival, currentPosition, desiredPosition;
-    int out;
-    int diff;
-
     switch(remex) {
-    case goTo: {
-        /*
-        regmap[POSITION_A_L] = (char) (positionA & 0xFF);
-        regmap[POSITION_A_H] = (char) (positionA >> 8);
-        */
-        currentPosition = combineInt(POSITION_A_L);
-        desiredPosition = combineInt(DES_POS_A_L);
-
-        diff = (desiredPosition - currentPosition);
-        pushIntegratorVal(diff);
-        ival = integratorRollingAverage();
-        if (abs(diff) <= abs(deadZone)) {
-            remex = halt;
-            set_PWM_A(0);
-        } else {
-            pval = diff;
-            pval *= gainxMult;
-            pval /= gainxDiv;
-            ival *= intxMult;
-            ival /= intxDiv;
-            out = pval + ival;
-            set_PWM_A(out);
-        }
+    case goTo:
         break;
-    }
     case zeroing:
-        set_PWM_A(-15);
         break;
     case zeroed:
-        splitInt(DES_POS_A_L, 50);
-        remex = goTo;
         break;
     case halt:
-        set_PWM_A(0);
+        break;
     }
-}
-
-int direction() {
-    int desiredPos = (int) (regmap[DES_POS_A_L] + (regmap[DES_POS_A_H] << 8));
-    int currentPos = (int) (regmap[POSITION_A_L] + (regmap[POSITION_A_H] << 8));
-
-    if (desiredPos > currentPos) {
-        return 1;
-    } else if (desiredPos < currentPos) {
-        return -1;
-    }
-    return 0;
 }
 
 // This function is called in an interrupt. Do not stall.
@@ -179,14 +77,6 @@ void onI2CCommand(unsigned const char cmd)
             remex = zeroing;
         }
     }
-
-    //set PID command
-    if (cmd == 5) {
-        gainxMult = regmap[PID_GAIN_MULT];
-        gainxDiv = regmap[PID_GAIN_DIV];
-        intxMult = regmap[PID_INT_MULT];
-        intxDiv = regmap[PID_INT_DIV];
-    }
 }
 
 void clear_registers(void)
@@ -196,12 +86,4 @@ void clear_registers(void)
         regmap[i] = 0x00;
     }
 }
-
-// Timer B0 interrupt service routine
-#pragma vector = TIMER1_B0_VECTOR
-__interrupt void Timer_B_1 (void)
-{
-    read_adc(CHANNEL_A);
-}
-
 
